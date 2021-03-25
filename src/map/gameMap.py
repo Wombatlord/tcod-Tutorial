e1cdd8d7f0c12cdf15f211840a0da189f0e96016
Wombatlord <jordan.stone0@gmail.com>
2021-03-25 04:06:31 +0000

    Added 'light' to represent in view tiles for adding FOV

--- src/map/gameMap.py
+++ src/map/gameMap.py
@@ -9,6 +9,12 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tileTypes.wall, order='F')
 
+        # Tiles currently in view.
+        self.visible = np.full((width, height), fill_value=False, order="F")
+
+        # Tiles the player has seen before.
+        self.explored = np.full((width, height), fill_value=False, order="F")
+
     def inBounds(self, x: int, y: int) -> bool:
         """Return True if x and y are within the bounds of this map"""
         return 0 <= x < self.width and 0 <= y < self.height
--- src/map/tileTypes.py
+++ src/map/tileTypes.py
@@ -5,18 +5,19 @@ import numpy as np  # type: ignore
 # Tile graphics structured type compatible with Console.tiles_rgb.
 graphicDt = np.dtype(
     [
-        ('ch', np.int32),  # Unicode codepoint.
-        ('fg', '3B'),  # 3 unsigned bytes for RGB.
-        ('bg', '3B'),
+        ("ch", np.int32),  # Unicode codepoint.
+        ("fg", "3B"),  # 3 unsigned bytes for RGB.
+        ("bg", "3B"),
     ]
 )
 
 # Tile struct used for statically defined tile data.
 tileDt = np.dtype(
     [
-        ('walkable', np.bool),  # True if this tile can be walked over.
-        ('transparent', np.bool),  # True if this tile doesn't block FOV.
-        ('dark', graphicDt),  # Graphics for tile out of FOV.
+        ("walkable", np.bool),  # True if this tile can be walked over.
+        ("transparent", np.bool),  # True if this tile doesn't block FOV.
+        ("dark", graphicDt),  # Graphics for tile out of FOV.
+        ("light", graphicDt),  # Graphics for tile in view.
     ]
 )
 
@@ -26,14 +27,24 @@ def newTile(
         walkable: int,
         transparent: int,
         dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+        light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """Helper function for defining individual tile types."""
-    return np.array((walkable, transparent, dark), dtype=tileDt)
+    return np.array((walkable, transparent, dark, light), dtype=tileDt)
 
+
+# SHROUD represents unexplored, unseen tiles.
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=tileDt)
 
 floor = newTile(
-    walkable=True, transparent=True, dark=(ord(' '), (255, 255, 255), (50, 50, 150))
+    walkable=True,
+    transparent=True,
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
 )
 wall = newTile(
-    walkable=False, transparent=False, dark=(ord(' '), (255, 255, 255), (0, 0, 100))
+    walkable=False,
+    transparent=False,
+    dark=(ord(' '), (255, 255, 255), (0, 0, 100)),
+    light=(ord(' '), (255, 255, 255), (130, 110, 50)),
 )
